8687e9b6a1bb4a0ce0d5bedade1fa9e0270b570e
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-05-15 01:46:04 +0700

    Add ILogger for future logging

--- SharedStatic.cs
+++ SharedStatic.cs
@@ -1,4 +1,5 @@
 ﻿using Hi3Helper.Plugin.Core.Management;
+using Microsoft.Extensions.Logging;
 #pragma warning disable CA2211
 
 namespace Hi3Helper.Plugin.Core
@@ -6,5 +7,6 @@ namespace Hi3Helper.Plugin.Core
     public static class SharedStatic
     {
         public static GameVersion LibraryStandardVersion = new(0, 1, 0, 0);
+        public static ILogger? InstanceLogger = null;
     }
 }
