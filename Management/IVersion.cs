72d26d430a3573760b535516d9f595a310be98f8
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-05-15 01:45:00 +0700

    Add IEquatable inherit to IVersion

--- Management/IVersion.cs
+++ Management/IVersion.cs
@@ -1,15 +1,35 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Hi3Helper.Plugin.Core.Management
 {
-    public interface IVersion
+    public interface IVersion : IEquatable<IVersion>
     {
         int get_Major();
         int get_Minor();
-        int get_Patch();
         int get_Build();
+        int get_Revision();
+
+        ReadOnlySpan<int> AsSpan();
 
         [return: MarshalAs(UnmanagedType.LPWStr)]
         string? ToString();
+        Version ToVersion();
+
+        public static bool operator >(IVersion? left, IVersion? right)
+        {
+            if (left == null || right == null)
+            {
+                return false;
+            }
+
+            return left.get_Major() > right.get_Major() ||
+                   (left.get_Major() == right.get_Major() && left.get_Minor() > right.get_Minor()) ||
+                   (left.get_Major() == right.get_Major() && left.get_Minor() == right.get_Minor() && left.get_Build() > right.get_Build()) ||
+                   (left.get_Major() == right.get_Major() && left.get_Minor() == right.get_Minor() && left.get_Build() == right.get_Build() && left.get_Revision() > right.get_Revision());
+        }
+
+        public static bool operator <(IVersion? left, IVersion? right) =>
+            right > left;
     }
 }
