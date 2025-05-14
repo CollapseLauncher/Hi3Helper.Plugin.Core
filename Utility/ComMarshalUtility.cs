7d6e684695c6b4b3f2a0c6151bdd09d0fb12fb11
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-05-15 01:45:52 +0700

    Initial PluginComUtility commit

---
+++ Utility/ComMarshalUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Hi3Helper.Plugin.Core.Utility
+{
+    public static class PluginComUtility
+    {
+        public static T[] CreateArrayFromSelector<T>(Func<int> countCallback, Func<int, T> selectorCallback)
+        {
+            int count = countCallback();
+            if (count == 0)
+            {
+                return [];
+            }
+
+            T[] values = new T[count];
+            for (int i = 0; i < count; i++)
+            {
+                values[i] = selectorCallback(i);
+            }
+
+            return values;
+        }
+    }
+}
