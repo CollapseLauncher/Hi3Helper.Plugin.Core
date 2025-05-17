eafced3c7dd9f54966f32dab41851bcd9d0055d1
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-05-17 14:25:14 +0700

    Fix conflicting Guid

--- ComInterfaceId.cs
+++ ComInterfaceId.cs
@@ -9,5 +9,5 @@ internal static class ComInterfaceId
     internal const string GameManager          = "436f6c6c-6170-7365-727f-000000000003";
     internal const string LauncherApiMedia     = "436f6c6c-6170-7365-727f-000000000004";
     internal const string LauncherApiNewsFeed  = "436f6c6c-6170-7365-727f-000000000005";
-    internal const string LauncherApiResources = "436f6c6c-6170-7365-727f-000000000005";
+    internal const string LauncherApiResources = "436f6c6c-6170-7365-727f-000000000006";
 }
