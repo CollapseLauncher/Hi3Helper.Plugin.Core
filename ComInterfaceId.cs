609fb8a3e41729a184fa44842caf319c243a4a71
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-06-01 18:32:57 +0700

    Make ComInterfaceId public

--- ComInterfaceId.cs
+++ ComInterfaceId.cs
@@ -2,16 +2,14 @@
 // ReSharper disable UnusedMember.Global
 namespace Hi3Helper.Plugin.Core;
 
-internal static class ComInterfaceId
+public static class ComInterfaceId
 {
-    internal const string ExInitializable        = "436f6c6c-6170-7365-727f-000000000000";
-
-    internal const string ExPlugin               = "436f6c6c-6170-7365-727f-000000000010";
-    internal const string ExPluginPresetConfig   = "436f6c6c-6170-7365-727f-000000000011";
-    internal const string ExGameManager          = "436f6c6c-6170-7365-727f-000000000012";
-
-    internal const string ExLauncherApi          = "436f6c6c-6170-7365-727f-000000000020";
-    internal const string ExLauncherApiMedia     = "436f6c6c-6170-7365-727f-000000000021";
-    internal const string ExLauncherApiNewsFeed  = "436f6c6c-6170-7365-727f-000000000022";
-    internal const string ExLauncherApiResources = "436f6c6c-6170-7365-727f-000000000023";
+    public const string ExInitializable        = "436f6c6c-6170-7365-727f-000000000000";
+    public const string ExPlugin               = "436f6c6c-6170-7365-727f-000000000010";
+    public const string ExPluginPresetConfig   = "436f6c6c-6170-7365-727f-000000000011";
+    public const string ExGameManager          = "436f6c6c-6170-7365-727f-000000000012";
+    public const string ExLauncherApi          = "436f6c6c-6170-7365-727f-000000000020";
+    public const string ExLauncherApiMedia     = "436f6c6c-6170-7365-727f-000000000021";
+    public const string ExLauncherApiNewsFeed  = "436f6c6c-6170-7365-727f-000000000022";
+    public const string ExLauncherApiResources = "436f6c6c-6170-7365-727f-000000000023";
 }
