00bee97116eb4f8c5148cc5c3e68385e2be67c0f
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-08-17 18:07:59 +0700

    Move TryGetApiExport from launcher to plugin

---
+++ Utility/PluginPInvokeExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Hi3Helper.Plugin.Core.Utility;
+
+public static class PluginPInvokeExtension
+{
+    public static unsafe bool TryGetExport<T>(this nint handle, string exportName, out T callback)
+        where T : Delegate
+    {
+        const string tryGetApiExportName = "TryGetApiExport";
+
+        Unsafe.SkipInit(out callback);
+
+        if (!NativeLibrary.TryGetExport(handle, tryGetApiExportName, out nint tryGetApiExportP) ||
+            tryGetApiExportP == nint.Zero)
+        {
+            return false;
+        }
+
+        delegate* unmanaged[Cdecl]<char*, void**, int> tryGetApiExportCallback =
+            (delegate* unmanaged[Cdecl]<char*, void**, int>)tryGetApiExportP;
+
+        nint  exportP     = nint.Zero;
+        char* exportNameP = exportName.GetPinnableStringPointer();
+        int   tryResult   = tryGetApiExportCallback(exportNameP, (void**)&exportP);
+
+        if (tryResult != 0 ||
+            exportP == nint.Zero)
+        {
+            return false;
+        }
+
+        callback = Marshal.GetDelegateForFunctionPointer<T>(exportP);
+        return true;
+    }
+}
