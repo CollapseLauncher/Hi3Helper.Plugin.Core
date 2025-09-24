a2ac98d4422abdaf2ca57a7cce71d2c890bd3eb0
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-09-25 00:55:27 +0700

    Bump API Standard version

--- SharedStatic.cs
+++ SharedStatic.cs
@@ -103,7 +103,7 @@ public class SharedStatic
     internal static string?                         ProxyPassword;
     internal static string                          PluginLocaleCode = "en-us";
 
-    public static readonly GameVersion LibraryStandardVersion = new(0, 1, 1, 0);
+    public static readonly GameVersion LibraryStandardVersion = new(0, 1, 2, 0);
     public static readonly ILogger     InstanceLogger         = new SharedLogger();
 
 #if DEBUG
