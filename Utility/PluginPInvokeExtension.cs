0b46a3707793a3660490ca68039f075fc50a1419
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-02-22 09:46:32 +0700

    Add TryGetExportUnsafe and fix TryGetApiExport returning error

--- Utility/PluginPInvokeExtension.cs
+++ Utility/PluginPInvokeExtension.cs
@@ -6,33 +6,42 @@ namespace Hi3Helper.Plugin.Core.Utility;
 
 public static class PluginPInvokeExtension
 {
-    public static unsafe bool TryGetExport<T>(this nint handle, string exportName, out T callback)
+    public static bool TryGetExport<T>(this nint handle, string exportName, out T callback)
         where T : Delegate
     {
-        const string tryGetApiExportName = "TryGetApiExport";
-
         Unsafe.SkipInit(out callback);
+        if (!TryGetExportAddressCore(handle, exportName, out nint exportP)) return false;
+
+        callback = Marshal.GetDelegateForFunctionPointer<T>(exportP);
+        return true;
+    }
+
+    public static bool TryGetExportUnsafe(this nint handle, string exportName, out nint callbackP)
+    {
+        Unsafe.SkipInit(out callbackP);
+        return TryGetExportAddressCore(handle, exportName, out callbackP);
+    }
+
+    private static unsafe bool TryGetExportAddressCore(
+        nint     handle,
+        string   exportName,
+        out nint exportP)
+    {
+        const string tryGetApiExportName = "TryGetApiExport";
 
         if (!NativeLibrary.TryGetExport(handle, tryGetApiExportName, out nint tryGetApiExportP) ||
             tryGetApiExportP == nint.Zero)
         {
+            exportP = 0;
             return false;
         }
 
-        delegate* unmanaged[Cdecl]<char*, void**, int> tryGetApiExportCallback =
-            (delegate* unmanaged[Cdecl]<char*, void**, int>)tryGetApiExportP;
+        delegate* unmanaged[Cdecl]<nint, out nint, int> tryGetApiExportCallback =
+            (delegate* unmanaged[Cdecl]<nint, out nint, int>)tryGetApiExportP;
 
-        nint  exportP     = nint.Zero;
-        char* exportNameP = exportName.GetPinnableStringPointer();
-        int   tryResult   = tryGetApiExportCallback(exportNameP, (void**)&exportP);
+        int tryResult = tryGetApiExportCallback(exportName.GetPinnableStringPointerSafe(), out exportP);
 
-        if (tryResult != 0 ||
-            exportP == nint.Zero)
-        {
-            return false;
-        }
-
-        callback = Marshal.GetDelegateForFunctionPointer<T>(exportP);
-        return true;
+        return tryResult == 0 &&
+               exportP != nint.Zero;
     }
 }
