3f41b59edcf12fa5b24ed3b42dc8e1d09d43508e
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-06-19 18:28:28 +0700

    Fix early registeredWaitHandle access on result finalization

--- Utility/ComAsyncExtension.cs
+++ Utility/ComAsyncExtension.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
-// ReSharper disable AccessToModifiedClosure
 namespace Hi3Helper.Plugin.Core.Utility;
 
 /// <summary>
@@ -48,38 +47,7 @@ public static partial class ComAsyncExtension
     /// <param name="resultP">A pointer of the <see cref="ComAsyncResult"/> struct.</param>
     /// <returns>An awaitable managed <see cref="Task"/>.</returns>
     public static Task AsTask(this nint resultP)
-    {
-        nint waitHandleP = ComAsyncResult.GetWaitHandle(resultP);
-
-        RegisteredWaitHandle? registeredWaitHandle = null;
-        TaskCompletionSource  tcs                  = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
-        SafeWaitHandle        safeHandle           = new SafeWaitHandle(waitHandleP, false);
-
-        WaitHandle waitHandle = new EventWaitHandle(false, EventResetMode.ManualReset)
-        {
-            SafeWaitHandle = safeHandle
-        };
-
-        registeredWaitHandle = ThreadPool.RegisterWaitForSingleObject(waitHandle, Impl, null, -1, true);
-
-        return tcs.Task;
-
-        unsafe void Impl(object? state, bool isTimedOut)
-        {
-            SetResult<TaskCompletionSource, nint>(resultP.AsPointer<ComAsyncResult>(), tcs);
-
-            safeHandle.Dispose();
-            waitHandle.Dispose();
-            ComAsyncResult.FreeResult(resultP);
-
-            if (waitHandleP != nint.Zero)
-            {
-                PInvoke.CloseHandle(waitHandleP);
-            }
-
-            registeredWaitHandle!.Unregister(null);
-        }
-    }
+        => new RegisteredWaitHandleContext(resultP).Task;
 
     /// <summary>
     /// Re-marshal a pointer of <see cref="ComAsyncResult"/> struct into a managed <see cref="Task"/>.
@@ -87,125 +55,166 @@ public static partial class ComAsyncExtension
     /// <param name="resultP">A pointer of the <see cref="ComAsyncResult"/> struct.</param>
     /// <returns>An awaitable managed <see cref="Task"/>.</returns>
     public static Task<T> AsTask<T>(this nint resultP)
-        where T : unmanaged
-    {
-        nint waitHandleP = ComAsyncResult.GetWaitHandle(resultP);
-
-        RegisteredWaitHandle?   registeredWaitHandle = null;
-        TaskCompletionSource<T> taskSource           = new(TaskCreationOptions.RunContinuationsAsynchronously);
-        SafeWaitHandle          safeHandle           = new SafeWaitHandle(waitHandleP, false);
-
-        WaitHandle waitHandle = new EventWaitHandle(false, EventResetMode.ManualReset)
-        {
-            SafeWaitHandle = safeHandle
-        };
-
-        registeredWaitHandle = ThreadPool.RegisterWaitForSingleObject(waitHandle, Impl, null, -1, true);
-
-        return taskSource.Task;
-
-        unsafe void Impl(object? state, bool isTimedOut)
-        {
-            SetResult<TaskCompletionSource<T>, T>(resultP.AsPointer<ComAsyncResult>(), taskSource);
-
-            safeHandle.Dispose();
-            waitHandle.Dispose();
-            ComAsyncResult.FreeResult(resultP);
-
-            if (waitHandleP != nint.Zero)
-            {
-                PInvoke.CloseHandle(waitHandleP);
-            }
-
-            registeredWaitHandle!.Unregister(null);
-        }
-    }
+        where T : unmanaged => (Task<T>)new RegisteredWaitHandleContext<T>(resultP).Task;
 
     /// <summary>
-    /// Sets result of the async operation based on its return type into the <see cref="TaskCompletionSource"/>.
+    /// A wait handle context to registers and returns a result once async operation is completed.
     /// </summary>
-    /// <typeparam name="TSource">A type of <see cref="TaskCompletionSource"/> or <see cref="TaskCompletionSource{TResult}"/></typeparam>
-    /// <typeparam name="T">A type of the return value from <see cref="ComAsyncResult"/>.</typeparam>
-    /// <param name="asyncResult">A pointer to the <see cref="ComAsyncResult"/> struct.</param>
-    /// <param name="tcs">An instance of <see cref="TaskCompletionSource"/> or <see cref="TaskCompletionSource{TResult}"/> to set the exception or result to.</param>
-    /// <exception cref="InvalidCastException">Whether the <typeparamref name="TSource"/> is not either <see cref="TaskCompletionSource"/> or <see cref="TaskCompletionSource{TResult}"/></exception>
-    private static unsafe void SetResult<TSource, T>(ComAsyncResult* asyncResult, TSource tcs)
-        where T : unmanaged
+    private class RegisteredWaitHandleContext
     {
-        // Try check the TCS kind.
-        switch (tcs)
+        protected nint                 AsyncResultP         { get; }
+        private   nint                 UnmanagedWaitHandleP { get; }
+        private   SafeWaitHandle       SafeWaitHandle       { get; }
+        private   WaitHandle           WaitHandle           { get; }
+        private   RegisteredWaitHandle RegisteredWaitHandle { get; }
+
+        private TaskCompletionSource TaskSourceResult { get; } = new(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        public virtual Task Task => TaskSourceResult.Task;
+
+        public RegisteredWaitHandleContext(nint asyncResultP)
         {
-            case TaskCompletionSource<T> asTcsRes:
-                SetResultInner(asyncResult, () => asTcsRes.SetResult(*(T*)asyncResult->_resultP), asTcsRes.SetException, asTcsRes.SetCanceled);
-                return;
-            case TaskCompletionSource asTcs:
-                SetResultInner(asyncResult, asTcs.SetResult, asTcs.SetException, asTcs.SetCanceled);
-                return;
+            AsyncResultP         = asyncResultP;
+            UnmanagedWaitHandleP = ComAsyncResult.GetWaitHandle(asyncResultP);
+
+            SafeWaitHandle = new SafeWaitHandle(UnmanagedWaitHandleP, false);
+            WaitHandle = new EventWaitHandle(false, EventResetMode.ManualReset)
+            {
+                SafeWaitHandle = SafeWaitHandle
+            };
+            RegisteredWaitHandle = ThreadPool.RegisterWaitForSingleObject(WaitHandle, SetResultCallback, this, -1, true);
         }
 
-        // Bada bing, bada bong. Throw the cast exception.
-        throw new InvalidCastException($"Cannot cast {nameof(TSource)} to either {nameof(TaskCompletionSource<>)} or {nameof(TaskCompletionSource)}");
-    }
-
-    private static unsafe void SetResultInner(ComAsyncResult* asyncResult, Action actionSetRes, SetExceptionDelegate actionSetExc, Action actionSetCancel)
-    {
-        // IsCancelled is set without IsFaulty being set. So, try check if the exception is not empty. If yes, then set the default exception.
-        if (asyncResult->IsCancelled)
+        private static void SetResultCallback(object? state, bool isTimedOut)
         {
-            if (asyncResult->ExceptionMemory.IsEmpty)
+            RegisteredWaitHandleContext ctx = (RegisteredWaitHandleContext)state!;
+            ctx.SetResultCallbackCore();
+        }
+
+        protected virtual unsafe void SetResultCallbackCore()
+        {
+            SetResult<TaskCompletionSource, nint>(AsyncResultP.AsPointer<ComAsyncResult>(), TaskSourceResult);
+            CloseHandles();
+        }
+
+        /// <summary>
+        /// Closes wait handles and free <see cref="ComAsyncResult"/> struct.
+        /// </summary>
+        protected void CloseHandles()
+        {
+            SafeWaitHandle.Dispose();
+            WaitHandle.Dispose();
+            ComAsyncResult.FreeResult(AsyncResultP);
+
+            if (UnmanagedWaitHandleP != nint.Zero)
             {
-                actionSetExc(new TaskCanceledException());
+                PInvoke.CloseHandle(UnmanagedWaitHandleP);
+            }
+
+            RegisteredWaitHandle.Unregister(null);
+        }
+
+        /// <summary>
+        /// Sets result of the async operation based on its return type into the <see cref="TaskCompletionSource"/>.
+        /// </summary>
+        /// <typeparam name="TSource">A type of <see cref="TaskCompletionSource"/> or <see cref="TaskCompletionSource{TResult}"/></typeparam>
+        /// <typeparam name="T">A type of the return value from <see cref="ComAsyncResult"/>.</typeparam>
+        /// <param name="asyncResult">A pointer to the <see cref="ComAsyncResult"/> struct.</param>
+        /// <param name="tcs">An instance of <see cref="TaskCompletionSource"/> or <see cref="TaskCompletionSource{TResult}"/> to set the exception or result to.</param>
+        /// <exception cref="InvalidCastException">Whether the <typeparamref name="TSource"/> is not either <see cref="TaskCompletionSource"/> or <see cref="TaskCompletionSource{TResult}"/></exception>
+        protected static unsafe void SetResult<TSource, T>(ComAsyncResult* asyncResult, TSource tcs)
+            where T : unmanaged
+        {
+            // Try check the TCS kind.
+            switch (tcs)
+            {
+                case TaskCompletionSource<T> asTcsRes:
+                    SetResultInner(asyncResult, () => asTcsRes.SetResult(*(T*)asyncResult->_resultP), asTcsRes.SetException, asTcsRes.SetCanceled);
+                    return;
+                case TaskCompletionSource asTcs:
+                    SetResultInner(asyncResult, asTcs.SetResult, asTcs.SetException, asTcs.SetCanceled);
+                    return;
+            }
+
+            // Bada bing, bada bong. Throw the cast exception.
+            throw new InvalidCastException($"Cannot cast {nameof(TSource)} to either {nameof(TaskCompletionSource<>)} or {nameof(TaskCompletionSource)}");
+        }
+
+        private static unsafe void SetResultInner(ComAsyncResult* asyncResult, Action actionSetRes, SetExceptionDelegate actionSetExc, Action actionSetCancel)
+        {
+            // IsCancelled is set without IsFaulty being set. So, try check if the exception is not empty. If yes, then set the default exception.
+            if (asyncResult->IsCancelled)
+            {
+                if (asyncResult->ExceptionMemory.IsEmpty)
+                {
+                    actionSetExc(new TaskCanceledException());
+                    return;
+                }
+
+                // Otherwise, set the exception from ExceptionMemory
+                SetException(asyncResult, actionSetExc, actionSetCancel);
                 return;
             }
 
-            // Otherwise, set the exception from ExceptionMemory
+            // If it isn't faulty, invoke result.
+            if (!asyncResult->IsFaulty)
+            {
+                actionSetRes();
+                return;
+            }
+
+            // Otherwise, invoke the exception.
             SetException(asyncResult, actionSetExc, actionSetCancel);
-            return;
         }
 
-        // If it isn't faulty, invoke result.
-        if (!asyncResult->IsFaulty)
+        /// <summary>
+        /// Set the exception provided by the <see cref="ComAsyncResult.ExceptionMemory"/>.
+        /// </summary>
+        /// <param name="asyncResult">A pointer to the <see cref="ComAsyncResult"/> struct.</param>
+        /// <param name="actionSetExc">A callback to set the exception into the <see cref="TaskCompletionSource"/></param>
+        /// <param name="actionSetCancel">A callback to set the canceled status into the <see cref="TaskCompletionSource"/></param>
+        /// <remarks>
+        /// If the buffer provided by the <see cref="ComAsyncResult.ExceptionMemory"/> is empty, then a generic <see cref="COMException"/> will be set.
+        /// </remarks>
+        private static unsafe void SetException(ComAsyncResult* asyncResult, SetExceptionDelegate actionSetExc, Action actionSetCancel)
         {
-            actionSetRes();
-            return;
-        }
+            StackTrace currentStackTrace = new(true);
+            Exception? exception         = ComAsyncException.GetExceptionFromHandle(asyncResult->ExceptionMemory);
 
-        // Otherwise, invoke the exception.
-        SetException(asyncResult, actionSetExc, actionSetCancel);
+#if DEBUG
+            Debug.Assert(exception != null, $"Exception shouldn't be null! Result address is: 0x{(nint)asyncResult:x8}");
+#endif
+
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+            if (exception == null)
+            {
+                actionSetExc(new COMException());
+                return;
+            }
+
+            exception.SetExceptionRemoteStackTrace() += Environment.NewLine;
+            exception.SetExceptionStackTrace()       =  currentStackTrace.ToString();
+            actionSetExc(exception);
+
+            if (exception is OperationCanceledException)
+            {
+                actionSetCancel();
+            }
+        }
     }
-
-    /// <summary>
-    /// Set the exception provided by the <see cref="ComAsyncResult.ExceptionMemory"/>.
-    /// </summary>
-    /// <param name="asyncResult">A pointer to the <see cref="ComAsyncResult"/> struct.</param>
-    /// <param name="actionSetExc">A callback to set the exception into the <see cref="TaskCompletionSource"/></param>
-    /// <param name="actionSetCancel">A callback to set the canceled status into the <see cref="TaskCompletionSource"/></param>
-    /// <remarks>
-    /// If the buffer provided by the <see cref="ComAsyncResult.ExceptionMemory"/> is empty, then a generic <see cref="COMException"/> will be set.
-    /// </remarks>
-    private static unsafe void SetException(ComAsyncResult* asyncResult, SetExceptionDelegate actionSetExc, Action actionSetCancel)
+    
+    /// <inheritdoc/>
+    private class RegisteredWaitHandleContext<T>(nint asyncResultP) : RegisteredWaitHandleContext(asyncResultP)
+        where T : unmanaged
     {
-        StackTrace currentStackTrace = new(true);
-        Exception? exception         = ComAsyncException.GetExceptionFromHandle(asyncResult->ExceptionMemory);
+        private TaskCompletionSource<T> TaskSourceResult { get; } = new(TaskCreationOptions.RunContinuationsAsynchronously);
 
-        #if DEBUG
-        Debug.Assert(exception != null, $"Exception shouldn't be null! Result address is: 0x{(nint)asyncResult:x8}");
-        #endif
+        public override Task Task => TaskSourceResult.Task;
 
-        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
-        if (exception == null)
+        protected override unsafe void SetResultCallbackCore()
         {
-            actionSetExc(new COMException());
-            return;
-        }
-
-        exception.SetExceptionRemoteStackTrace() += Environment.NewLine;
-        exception.SetExceptionStackTrace()       =  currentStackTrace.ToString();
-        actionSetExc(exception);
-
-        if (exception is OperationCanceledException)
-        {
-            actionSetCancel();
+            SetResult<TaskCompletionSource<T>, T>(AsyncResultP.AsPointer<ComAsyncResult>(), TaskSourceResult);
+            CloseHandles();
         }
     }
 }
